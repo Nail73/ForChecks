81ddc2000beb425201a54bc1bd1706f065a875b5
Nail Fakhrtdinov <fakh-nail@yandex.ru>
2023-07-13 13:44:56 +0400

    Spring app

---
+++ spring/src/main/java/com/aston/spring/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.aston.spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String showStatus() {
+        return "Hello world!";
+    }
+}
